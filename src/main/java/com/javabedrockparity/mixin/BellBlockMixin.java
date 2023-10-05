e7e908b0ad6787e48c30e8b13a8bf232f514f28d
ItsRemurin <ItsRemurin@gmail.com>
2023-10-05 18:54:53 -0400

    Fix for bells ring

--- src/main/java/com/javabedrockparity/mixin/BellBlockMixin.java
+++ src/main/java/com/javabedrockparity/mixin/BellBlockMixin.java
@@ -1,17 +1,10 @@
 package com.javabedrockparity.mixin;
-
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.BellBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.TargetBlock;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,16 +12,15 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(BellBlock.class)
 public abstract class BellBlockMixin extends BlockWithEntity {
+    @Shadow public abstract boolean ring(World world, BlockState state, BlockHitResult hitResult, @Nullable PlayerEntity player, boolean checkHitPos);
+
     protected BellBlockMixin(Settings settings) {
         super(settings);
     }
 
-    @Shadow public abstract boolean ring(World world, BlockPos pos, @Nullable Direction direction);
 
     @Override
     public void onProjectileHit(World world, BlockState state, BlockHitResult hit, ProjectileEntity projectile) {
-
-        ring(world, hit.getBlockPos(), hit.getSide());
-        projectile.setVelocity(projectile.getVelocity().x * -1, projectile.getVelocity().y / 2, projectile.getVelocity().z * -1);
+        ring(world, state, hit, projectile.getOwner() != null && projectile.getOwner().isPlayer() ? (PlayerEntity) projectile.getOwner() : null, true);
     }
 }
