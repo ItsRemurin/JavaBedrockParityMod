a4b658f4b04de75f2caecc9bf10fab1a26794a87
ItsRemurin <ItsRemurin@gmail.com>
2023-10-05 21:31:16 -0400

    Snow now decays after snowfall

---
+++ src/main/java/com/javabedrockparity/mixin/ServerWorldMixin.java
@@ -0,0 +1,54 @@
+package com.javabedrockparity.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.RandomSequencesState;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.world.spawner.Spawner;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+@Mixin(ServerWorld.class)
+public abstract class ServerWorldMixin extends World{
+    protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
+    }
+
+    @Inject(method = "tickIceAndSnow", at = @At("TAIL"))
+    public void tickIceAndSnow(boolean raining, BlockPos pos, CallbackInfo ci) {
+        if(!raining) {
+            BlockPos blockPos = this.getTopPosition(Heightmap.Type.MOTION_BLOCKING, pos);
+            BlockState blockState = getBlockState(pos);
+            if (blockState.isOf(Blocks.SNOW)) {
+                int j = blockState.get(SnowBlock.LAYERS);
+                if (j > 0) {
+                    BlockState blockState2 = blockState.with(SnowBlock.LAYERS, j - 1);
+                    this.setBlockState(blockPos, blockState2);
+                }
+            }
+        }
+    }
+}
